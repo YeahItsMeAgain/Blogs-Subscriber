0436d55ede99591b0af54e0af1e8f32dc5b18c3a
Mevaser <mevaser.rotner@gmail.com>
2023-05-13 18:08:08 +0300

    Bulk subscribe\unsubscribe to blogs

--- bot/handlers/blog.go
+++ bot/handlers/blog.go
@@ -32,24 +32,29 @@ func HandleUnsubscribe(ctx telebot.Context) error {
 }
 
 func HandleBlogRequest(ctx telebot.Context) error {
-	blogUrl := ctx.Text()
-	_, err := url.ParseRequestURI(blogUrl)
-	if err != nil {
-		return ctx.Send(fmt.Sprintf("❎ %s is not a valid url", blogUrl))
-	}
+	blogsUrls := ctx.Text()
+	for _, blogUrl := range strings.Split(blogsUrls, "\n") {
+		_, err := url.ParseRequestURI(blogUrl)
+		if err != nil {
+			ctx.Send(fmt.Sprintf("❎ %s is not a valid url", blogUrl))
+			continue
+		}
 
-	user := utils.GetCurrentUser(ctx)
-	blog := models.Blog{Url: blogUrl}
-	db.DB.Preload(clause.Associations).FirstOrCreate(&blog, "url = ?", blogUrl)
-	if !funk.Contains(blog.Subscribers, func(subscriber *models.User) bool {
-		return subscriber.ID == user.ID
-	}) {
-		db.DB.Model(&blog).Association("Subscribers").Append(&user)
-		log.Printf("[*] %d: %s Subscribed to: %s.", ctx.Sender().ID, ctx.Sender().Username, ctx.Text())
-		return ctx.Send(fmt.Sprintf("✅ You have successfully subscribed to: %s", blogUrl))
-	}
+		user := utils.GetCurrentUser(ctx)
+		blog := models.Blog{Url: blogUrl}
+		db.DB.Preload(clause.Associations).FirstOrCreate(&blog, "url = ?", blogUrl)
+		if !funk.Contains(blog.Subscribers, func(subscriber *models.User) bool {
+			return subscriber.ID == user.ID
+		}) {
+			db.DB.Model(&blog).Association("Subscribers").Append(&user)
+			log.Printf("[*] %d: %s Subscribed to: %s.", ctx.Sender().ID, ctx.Sender().Username, ctx.Text())
+			ctx.Send(fmt.Sprintf("✅ You have successfully subscribed to: %s", blogUrl))
+			continue
+		}
 
-	log.Printf("[*] %d: %s Unsubscribed from: %s.", ctx.Sender().ID, ctx.Sender().Username, ctx.Text())
-	db.DB.Model(&blog).Association("Subscribers").Delete(&user)
-	return ctx.Send(fmt.Sprintf("✅ You have successfully unsubscribed from: %s", blogUrl))
+		log.Printf("[*] %d: %s Unsubscribed from: %s.", ctx.Sender().ID, ctx.Sender().Username, ctx.Text())
+		db.DB.Model(&blog).Association("Subscribers").Delete(&user)
+		ctx.Send(fmt.Sprintf("✅ You have successfully unsubscribed from: %s", blogUrl))
+	}
+	return nil
 }
